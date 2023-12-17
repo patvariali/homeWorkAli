3bc348f72acb18512a8f541d284b8c520313fd59
patvariali <patvariali2@gmail.com>
2023-12-16 21:05:14 -0600

    day 28 hw

--- src/day28/HW/TestCircleObj.java
+++
@@ -1,8 +0,0 @@
-package day28.HW;
-
-public class TestCircleObj {
-    public static void main(String[] args) {
-        Candy candy = new Candy("Chupa Chups", 1, 0, true);
-        System.out.println(candy);
-    }
-}
